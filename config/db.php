cac7495eb11f84db9c84747b0d6c9dc999e0ed41
wibiesana <wibiesana@yahoo.com>
2020-04-26 13:39:22 +0700

    init

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yourDatabase',
+    'dsn' => 'mysql:host=localhost;dbname=db_ayam',
     'username' => 'root',
-    'password' => '',
+    'password' => 'wibie',
     'charset' => 'utf8',
 ];
