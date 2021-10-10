ae2a5b80d0227dccd264f88227271f17137aba37
wibiesana <wibiesana@yahoo.com>
2021-10-10 08:47:30 +0700

    edit config

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2micro',
     'username' => 'root',
-    'password' => 'wibie',
+    'password' => '',
     'charset' => 'utf8',
 ];
